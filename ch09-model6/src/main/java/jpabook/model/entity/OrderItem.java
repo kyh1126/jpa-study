b0ec722d34551267478549db6b511c541318eb25
jenny <yunhee.kim@smartfoodnet.com>
2021-10-23 01:10:20 +0900

    Chapter 9. 값 타입

---
+++ ch09-model6/src/main/java/jpabook/model/entity/OrderItem.java
@@ -0,0 +1,84 @@
+package jpabook.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import jpabook.model.entity.item.Item;
+
+/**
+ * Created by holyeye on 2014. 3. 11..
+ */
+@Entity
+@Table(name = "ORDER_ITEM")
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ITEM_ID")
+    private Item item;      //주문 상품
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ORDER_ID")
+    private Order order;    //주문
+
+    private int orderPrice; //주문 가격
+    private int count;      //주문 수량
+
+    //Getter, Setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public int getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(int buyPrice) {
+        this.orderPrice = buyPrice;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem{" +
+                "id=" + id +
+                ", buyPrice=" + orderPrice +
+                ", count=" + count +
+                '}';
+    }
+}
